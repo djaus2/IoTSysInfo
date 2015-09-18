f74c50171f09bdeb9375cea822852f1628b3964c
djaus2 <davidjones@sportronics.com.au>
2015-09-18 14:00:16 +1000

    First upload
    Upload of first version

---
+++ SysInfo/MainPage.xaml.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SysInfo
+{
+    /// <summary>
+    /// Based upon Bruce Eitman's blog:
+    /// http://embedded101.com/BruceEitman/entryid/676/Windows-10-IoT-Core-Getting-the-MAC-Address-from-Raspberry-Pi.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        ///Used for drilling into list items.
+        ///Some query response lists are simplified by only showing one item per entity.
+        ///Eg Processes returns multiple properties for a process
+        ///Only show the ImageName/Decription/Name propertty then allow to drill in for other properties by clicking on the item.
+        /// </summary>
+        private enum Instate
+        {
+            None,
+            Ipconfig, //eg. Have listed network devices
+            Ipconfig_Aninterface, //eg. Have selected a network device and displayed its properties
+            Processes,
+            Processes_Aprocess,
+            Devices,
+            Devices_Adevice,
+            Providers,
+            Providers_Aprovider
+        }
+        Instate SystemState = Instate.None;
+
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SystemState = Instate.None;
+        }
+
+        private void buttonSelectTarget_Click(object sender, RoutedEventArgs e)
+        {
+            Button buttonTemp = (Button)sender;
+            textBoxDevice.Text = buttonTemp.Content.ToString();
+        }
+
+        private void textBoxTargetDeviceName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SysInfo.Device = textBoxDevice.Text;
+        }
+
+        private void textBoxPort_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SysInfo.Port = textBoxPort.Text;
+        }
+
+        private void textBoxUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SysInfo.Admin = textBoxUser.Text;
+        }
+
+        private void textBoxPwd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SysInfo.Pwd = textBoxPwd.Text;
+        }
+
+        private string lastButton = "";
+        /// <summary>
+        /// Action a command
+        /// </summary>
+        /// <param name="sender">The command button. Its content(text) is the actual parameter</param>
+        private async void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Get the "actual" parameter from button content
+            Button button = (Button)sender;
+            string buttonText = ((string)button.Content).ToLower();
+
+            if (buttonText == "clear params")
+            {
+                textBoxAPI_Params.Text="";
+                return;
+            }
+
+            if (buttonText != lastButton)
+            {
+                //Do anything required when a different command is pressed ??
+                lastButton = buttonText;
+            }
+
+            //New command so clear everything
+            SystemState = Instate.None;
+            NameValue.NameValuesStack.Clear();
+            DeviceInterfacesOutputList.SelectedIndex = -1;
+            //Th erest commands can use parameters
+            SysInfo.API_Params = textBoxAPI_Params.Text;
+
+            //Lookup the command's REST URL
+            string url = "";
+            switch (buttonText)
+            {
+                case "ipconfig":
+                    url = SysInfo.IpConfigURL;
+                    break;
+                case "sysinfo":
+                    url = SysInfo.SysInfoURLRL;
+                    break;
+                case "api":
+                    url = SysInfo.APIURL;
+                    break;
+                case "osapi":
+                    url = SysInfo.OSAPILRL;
+                    break;
+                case "devices":
+                    url = SysInfo.DevicesURL;
+                    break;
+                case "processes":
+                    url = SysInfo.ProcessesURL;
+                    break;
+                case "default app":
+                    url = SysInfo.DefaulAppURL;
+                    break;
+                case "providers":
+                    url = SysInfo.ProvidersURL;
+                    break;
+            }
+
+            //Show this in the MainPage URL textbox
+            //API buttomn actions what ever is here.
+            this.textBoxAPI.Text = url;
+
+            //Do the REST query and JSON parsing
+            bool res = await SysInfo.DoQuery(url);
+
+            //If not OK then only show a generic error message
+            if (!res)
+            {
+                NameValue.ClearList();
+                NameValue nv = new NameValue("Error:", "Target not found, timeout or processing error.");
+            }
+            else
+            {
+                //If the query response list is from an array simplify by only showing one entry in the list per item
+                //ie Only show the item name/description etc.
+                if (NameValue.NameValues_IsFrom_Array)
+                {
+                    switch (buttonText)
+                    {
+                        //ToDo: Could pattern this.
+                        case "ipconfig":
+                            NameValue.NameValuesStack.Push(NameValue.NameValues);
+                            var dt0 = from d0 in NameValue.NameValues where d0.Name.Contains("Description") select d0;
+                            NameValue.NameValues = dt0.ToList<NameValue>();
+                            SystemState = Instate.Processes;
+                            break;
+                        case "processes":
+                            NameValue.NameValuesStack.Push(NameValue.NameValues);
+                            var dt = from d in NameValue.NameValues where d.Name.Contains("ImageName") select d;
+                            NameValue.NameValues = dt.ToList<NameValue>();
+                            SystemState = Instate.Processes;
+                            break;
+                        case "devices":
+                            NameValue.NameValuesStack.Push(NameValue.NameValues);
+                            var dt2 = from d2 in NameValue.NameValues where d2.Name.Contains("Description") select d2;
+                            NameValue.NameValues = dt2.ToList<NameValue>();
+                            SystemState = Instate.Devices;
+                            break;
+                        case "providers":
+                            NameValue.NameValuesStack.Push(NameValue.NameValues);
+                            var dt3 = from d3 in NameValue.NameValues where d3.Name.Contains("Name") select d3;
+                            NameValue.NameValues = dt3.ToList<NameValue>();
+                            SystemState = Instate.Devices;
+                            break;
+                    }
+                }
+            }
+
+            DeviceInterfacesOutputList.DataContext = NameValue.NameValues;
+        }
+
+        /// <summary>
+        /// Can provide parameters to URL
+        /// </summary>
+        private void textBoxAPI_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SysInfo.APIURL = textBoxAPI.Text;
+        }
+
+        /// <summary>
+        /// Slider sets the query timeout 0.5 to 20sec.
+        /// </summary>
+        private void slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            double timeout = e.NewValue;
+            SysInfo.Timeout = (int) timeout;
+            timeout = timeout / 1000;
+            textBoxTimeout.Text = timeout.ToString();
+        }
+
+        /// <summary>
+        /// Cancel the query
+        /// </summary>
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            SysInfo.cts.Cancel();
+        }
+
+        /// <summary>
+        /// Drill into an item to get selected item's properties
+        /// </summary>
+        private void DeviceInterfacesOutputList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (SystemState)
+            {
+                case Instate.Ipconfig:
+                case Instate.Processes:
+                case Instate.Devices:
+                case Instate.Providers:
+                    //This HAS been patterned:
+                    //Name contains a dotted index. Need to select all such items from original list
+                    NameValue nv = (NameValue)DeviceInterfacesOutputList.SelectedItem;
+                    string name = nv.Name;
+                    int index = name.IndexOf(" ");
+                    string indexStr = name.Substring(0,index);
+                    NameValue.NameValues = NameValue.NameValuesStack.Pop();
+                    var dt = from d in NameValue.NameValues where d.Name.Substring(0,index) == indexStr select d;
+                    
+                    //Could implement back button. ToDo would use:
+                    NameValue.NameValuesStack.Push(NameValue.NameValues);
+
+                    NameValue.NameValues = dt.ToList<NameValue>();
+                    switch (SystemState)
+                    {
+                        case Instate.Ipconfig:
+                            SystemState = Instate.Ipconfig_Aninterface;
+                            break;
+                        case Instate.Processes:
+                            SystemState = Instate.Processes_Aprocess;
+                            break;
+                        case Instate.Devices:
+                            SystemState = Instate.Devices_Adevice;
+                            break;
+                        case Instate.Providers:
+                            SystemState = Instate.Providers_Aprovider;
+                            break;
+                    }
+                    DeviceInterfacesOutputList.DataContext = NameValue.NameValues;
+                    break;
+            }
+        }
+    }
+}
---
+++ SysInfo/SysInfo.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+using Windows.Data.Json;
+
+
+namespace SysInfo
+{
+    /// <summary>
+    /// JSON returns Name-Value pairs
+    /// When show results of queries, create a NameValue obkect for each JSON name-value object
+    /// The List binds to teh Name and Vlaue property of NameValue object list (NameValue.NameValues).
+    /// </summary>
+    public class NameValue
+    {
+        //Properties
+        public string Name { get; set;}
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Standard constructor
+        /// </summary>
+        /// <param name="name">JSON name of properfty</param>
+        /// <param name="value">JSON value of property</param>
+        public NameValue(string name, string value)
+        {
+            Name = name;
+            Value = value;
+            //Objects are automatically addded to list.
+            NameValues.Add(this);
+        }
+
+        /// <summary>
+        /// Create obvject from JSON obkect
+        /// </summary>
+        /// <param name="oJsn">The JSON obkect</param>
+        public NameValue(JsonObject oJsn)
+        {
+            string name;
+            name = oJsn.Keys.ToArray<string>()[0];
+            IJsonValue val;
+            bool res = oJsn.TryGetValue(name, out val);
+            Name = name;
+            String Type = oJsn.GetNamedString("Type");
+            Value = val.ToString();
+
+            NameValues.Add(this);
+        }
+
+        /// <summary>
+        /// Create object from JSON KeyValuePair
+        /// This is what is used below
+        /// </summary>
+        /// <param name="preName">Higher level info about this branch</param>
+        /// <param name="index">If an array item apply index to prepend of name</param>
+        /// <param name="oJsn">The </param>
+        public NameValue(string preName, int index, KeyValuePair<string, IJsonValue> oJson_KVP)
+        {
+            Name = oJson_KVP.Key;
+            //Display a property so inndicate with ,
+            if (preName != "")
+                Name = preName +"."  + oJson_KVP.Key;
+            IJsonValue val = oJson_KVP.Value;
+            Value = val.ToString();
+            //For string values remove leading and trailing "
+            //ToDo: Could make this an option
+            Value = Value.Replace("\"", "");
+            NameValues.Add(this);
+        }
+
+        /// <summary>
+        /// This is the "Powerhouse"
+        /// Get all name-value pairs in the object
+        /// - For each value get its value if simple type
+        /// - If value is an array get objects in array an recursively call this for each item in the array
+        /// - If value is an object call this recursiveky.
+        /// </summary>
+        /// <param name="preName">Text to prepend to name in list</param>
+        /// <param name="oJson">The JSON object</param>
+        public static void GetNameValues(string preName, JsonObject oJson)
+        {
+            foreach (KeyValuePair<string, IJsonValue> oJson_KVP in oJson)
+            {
+                string prefix = oJson_KVP.Key;
+                if (preName != "")
+                    //Drilling into an object to indicate using ->
+                    prefix = preName + "->" + oJson_KVP.Key;
+
+                //A bit of house keeping: List names are in plural.
+               // int n = "address".Length;
+               // if (prefix.Substring(prefix.Length - n, n).ToLower() != "address")
+               // {
+               //     if (prefix.Substring(prefix.Length - 2, 2) == "es")
+               //         prefix = prefix.Substring(0, prefix.Length - 2);
+               //     else if (prefix.Substring(prefix.Length - 1, 1) == "s")
+               //         prefix = prefix.Substring(0, prefix.Length - 1);
+               //}
+
+                string typ = oJson_KVP.Value.ValueType.ToString().ToLower();
+
+                if (typ == "object")
+                {
+                    //Recursively call this for the value
+                    GetNameValues(prefix , oJson.GetNamedObject(oJson_KVP.Key));
+                }
+                else if (typ == "array")
+                {            
+                    JsonArray ja =  oJson.GetNamedArray(oJson_KVP.Key);
+                    //foreach (JsonObject jajo in ja.)
+                    for(uint index = 0; index < ja.Count; index++)
+                    {
+                        string prfx = prefix;
+                        if (ja.Count > 1)
+                        {
+                            //If more than one item in the array, prepend the array index of the item  to the name,
+                            NameValues_IsFrom_Array = true;
+                            prfx = index.ToString() + ". " + prfx;
+                        }
+                        JsonObject joja = ja.GetObjectAt(index).GetObject();
+                        //Recursively call this for the values in teh array
+                        GetNameValues(prfx, joja);
+                    }
+                }
+                else
+                {
+                    //Got a value to add to list
+                    NameValue nv = new NameValue(preName,-1, oJson_KVP);
+                }
+
+            }
+        }
+
+        public static void ClearList()
+        {
+            NameValues = new List<NameValue>();
+            NameValues_IsFrom_Array = false;
+        }
+
+        /// <summary>
+        /// This is the list that is bound to the listbox
+        /// </summary>
+        public static List<NameValue> NameValues = new List<NameValue>();
+
+        //Where the items are an array, collapse to one entry per item but flag that so it can be interogated.
+        public static bool NameValues_IsFrom_Array { get; set; } = false;
+        //Stack used top preserve list that are collapsed. Used when "drilling into" an item's properties.
+        public static Stack<List<NameValue>> NameValuesStack = new Stack<List<NameValue>>();
+    }
+
+    /// <summary>
+    /// Performs queries to Win 10 IoT device's web interface
+    /// Uses REST Interface
+    /// Makes use of NameValue class to do treturned JSON processing
+    /// </summary>
+    public static class SysInfo {
+
+        //Device credentials
+        public static string Device { get; set; } = "localhost";
+        public static string Port { get; set; } = "8080";
+        public static string Admin { get; set; } = "Administrator";
+        public static string Pwd { get; set; } = "p@ssw0rd";
+
+        //Query strings
+        public static string IpConfigURL { get; set; } = "api/networking/ipconfig";
+        public static string SysInfoURLRL { get; set; } = "api/iot/deviceinformation";
+        public static string APIURL { get; set; } = "api/networking/ipconfig";
+        public static string OSAPILRL { get; set; } = "api/os/info";
+        public static string DevicesURL { get; set; } = "api/devicemanager/devices";
+        public static string ProcessesURL { get; set; } = "api/resourcemanager/processes";
+        public static string DefaulAppURL { get; set; } = "api/iot/appx/getdefault";
+        public static string ProvidersURL { get; set; } = "/api/etw/providers";
+        public static string API_Params { get; set; } = "";
+
+
+
+        public async static Task<bool>  DoQuery(string url)
+        {
+            
+            NameValue.ClearList();
+
+            //Following was moved to MainPage as need thsi as state.
+            //string url = "";
+            //switch (sender.ToLower())
+            //{
+            //    case "ipconfig":
+            //        url = IpConfigURL;
+            //        break;
+            //    case "sysinfo":
+            //        url = SysInfoURLRL;
+            //        break;
+            //    case "api":
+            //        url = APIURL;
+            //        break;
+            //    case "osapi":
+            //        url = OSAPILRL;
+            //        break;
+            //    case "devices":
+            //        url = DevicesURL;
+            //        break;
+            //    case "processes":
+            //        url = ProcessesURL;
+            //        break;
+            //    case "default app":
+            //        url = DefaulAppURL;
+            //        break;
+            //}
+
+
+            //The REST call for the command
+            StreamReader SR = await GetJsonStreamData(url);
+            if (SR==null)
+            {
+                return false;
+            }
+
+            //Process the JSON stream data
+            JsonObject ResultData = null;
+            try
+            {
+                String JSONData;
+                //Get the stream as text.
+                JSONData = SR.ReadToEnd();
+
+                //Convert to JSON object
+                ResultData = (JsonObject)JsonObject.Parse(JSONData); 
+
+                //Process the JSON data
+                NameValue.GetNameValues("", ResultData);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public static CancellationTokenSource cts { get; set; } = new CancellationTokenSource();
+        public static int Timeout { get; set; } = 10000;
+
+        /// <summary>
+        /// The command REST call to the Win 10 IoT device
+        /// </summary>
+        /// <param name="URL">The REST URL less the host information</param>
+        /// <returns>The REST stream. Returns null if any errors etc.</returns>
+        private static async Task<StreamReader> GetJsonStreamData(String URL)
+        {
+            HttpWebRequest wrGETURL = null;
+            Stream objStream = null;
+            StreamReader objReader = null; //If any errors etc this is returned as null.
+            string url = "http://" + Device + ":" + Port +"/"+ URL;
+
+            //Can add parameters:
+            if (API_Params != "")
+            {
+                API_Params = API_Params.Trim();
+                if (API_Params[0] != '?')
+                    API_Params = "?" + API_Params;
+                url += API_Params;
+            }
+
+            try
+            {
+                //Set up REST call
+                wrGETURL = (HttpWebRequest)WebRequest.Create(url);
+                wrGETURL.Credentials = new NetworkCredential(Admin,Pwd);
+
+                //Use ability to cancel/timeout/capture error
+                HttpWebResponse Response = null; // = (HttpWebResponse)(await wrGETURL.GetResponseAsync());
+                bool completed = false;
+                cts = new CancellationTokenSource();
+                try
+                {
+                    cts.CancelAfter(Timeout);
+                    //Make teh REST call:
+                    Response = (HttpWebResponse)(await wrGETURL.GetResponseAsync());
+                    completed = true;
+                }
+                catch (TaskCanceledException ex)
+                {
+                    completed = false;
+                }
+                catch (Exception exception)
+                {
+                    completed = false;
+                }
+
+                if ( (!cts.IsCancellationRequested) && (completed))
+                {
+                    if (Response.StatusCode == HttpStatusCode.OK)
+                    {
+                        //If OK then get reader to return.
+                        objStream = Response.GetResponseStream();
+                        objReader = new StreamReader(objStream);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("GetData " + e.Message);
+            }
+            return objReader;
+        }
+
+    }
+}
